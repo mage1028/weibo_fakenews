a0d557de2e9ac457d5d2b22280d982fee852bb7b
mage1028 <524538131@qq.com>
2018-06-01 17:48:35 +0800

    fix bug

--- weibo_spider/spiders/spider.py
+++ weibo_spider/spiders/spider.py
@@ -15,7 +15,7 @@ import logging
 class FakeNewsSpider(Spider):
     name = 'fakenews'
     urls = []
-    for i in range(322, 1000):
+    for i in range(322, 600):
         urls.append(['https://service.account.weibo.com/?type=5&status=4&page={0}'.format(i)])
 
     def start_requests(self):
